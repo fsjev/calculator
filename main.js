60f9ff6a40c0fd17c6fb45d2c7e1cb731f32080d
Jevaughn Vickers <fullstackjev@gmail.com>
2023-06-27 14:00:42 -0400

    create function to decide which operation to use

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+// global variables
+let firstOperand = 15;
+let secondOperand = 5;
+let operator = "-";
+
+
 function add(x, y){
     return x + y;
 };
@@ -13,3 +19,18 @@ function multiply(x, y){
 function divide(x, y){
     return x / y;
 };
+
+function operate(firstOperand, secondOperand, operator){
+    switch(operator){
+        case "+":
+            return add(firstOperand, secondOperand);
+        case "-":
+            return subtract(firstOperand, secondOperand);
+        case "*":
+            return multiply(firstOperand, secondOperand);
+        case "/":
+            return divide(firstOperand, secondOperand);
+    };
+};
+
+console.log(operate(firstOperand,secondOperand,operator));
