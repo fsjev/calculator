c9ff0f694930ce6030f24450bcaedd66ec60481b
Jevaughn Vickers <fullstackjev@gmail.com>
2023-06-28 17:02:44 -0400

    modify UI

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 // global variables
-let firstOperand = 15;
-let secondOperand = 5;
-let operator = "-";
+let firstOperand = 0;
+let secondOperand = 0;
+let operator = "";
+let display = document.querySelector(".display");
+let buttons = document.querySelectorAll(".key");
+let clearAllButton = document.querySelector(".clear-all");
+let clearButton = document.querySelector(".clear");
+let zeroButton = document.querySelector(".zero");
+let equalButton = document.getElementById("equal");
+let plusButton = document.getElementById("add");
+let minusButton = document.getElementById("subtract");
+let multiplyButton = document.getElementById("multiply");
+let divideButton = document.getElementById("divide");
+let negButton = document.getElementById("negative");
+let squareButton = document.getElementById("square");
+let decimalButton = document.getElementById("decimal");
+let keysContainer = document.querySelector(".keys-container");
 
 
 function add(x, y){
@@ -20,6 +34,10 @@ function divide(x, y){
     return x / y;
 };
 
+function square(x){
+    return x ** 2;
+};
+
 function operate(firstOperand, secondOperand, operator){
     switch(operator){
         case "+":
@@ -33,4 +51,121 @@ function operate(firstOperand, secondOperand, operator){
     };
 };
 
-console.log(operate(firstOperand,secondOperand,operator));
+buttons.forEach(button => button.addEventListener("click", () => {
+    let liveOperator = keysContainer.getElementsByClassName("operator-click");
+    if(display.textContent.length <= 12){
+        if(button === clearAllButton){
+            clearDisplay();
+            clearOpVariabes();
+        }else if(button === multiplyButton){
+            // if(liveOperator.length === 0){
+            //     multiplyButton.setAttribute("class", "key operator operator-click");
+            // }else{
+            //     multiplyButton.setAttribute("class", "key operator");
+            // }
+            firstOperand = display.textContent;
+        }
+        
+        
+        else if(button === clearButton){
+            if(display.textContent.length === 1){
+                display.textContent = 0;
+            }else{
+                display.textContent = clearLast();
+            }
+        }else if(button === negButton){
+            if(display.textContent.includes("-")){
+                display.textContent = removeNegative();
+            }else{
+                display.textContent = makeNegative();
+            }
+        }else if(button === squareButton){
+            if(display.textContent !== "0"){
+                firstOperand = display.textContent;
+                display.textContent = square(firstOperand);
+
+            }
+        }
+        
+        
+        else if(button === decimalButton){
+            if(!display.textContent.includes(".")){
+                display.textContent = `${display.textContent}${button.textContent}`;
+            }else{
+                display.textContent = clearLast();
+            }
+            
+        }
+        
+        
+        
+        else if(button === equalButton){
+            // outputResult();
+        }else if(button === zeroButton){
+            if(display.textContent === "0"){
+                display.textContent === 0;
+            }else{
+                display.textContent = `${display.textContent}${button.textContent}`;
+            }
+        }else{
+            if(display.textContent === "0"){
+                display.textContent = button.textContent;
+            }else if(display.textContent === "-0"){
+                display.textContent = button.textContent;
+            }
+            
+            
+            else{
+                display.textContent = `${display.textContent}${button.textContent}`;
+            }
+        }
+    }else{
+        if(button === multiplyButton && button === divideButton && button === plusButton && button === minusButton){
+            console.log("yap");
+            // code for processing opers
+        }else if(button === clearAllButton){
+            clearDisplay();
+            clearOpVariabes();
+        }else if(button === clearButton){
+            if(display.textContent.length === 1){
+                display.textContent = 0;
+            }else{
+                display.textContent = clearLast();
+            }
+        }   
+    }
+}));
+
+
+function clearDisplay(){
+    display.textContent = 0;
+};
+
+function clearOpVariabes(){
+    firstOperand = 0;
+    secondOperand = 0;
+    operator = "";
+    
+};
+
+function clearLast(){
+    let str = display.textContent;
+    let array = Array.from(str);
+    array.pop()
+    return array.join("");
+};
+
+function makeNegative(){
+    let str = display.textContent;
+    let array = Array.from(str);
+    array.splice(0, 0, "-");
+    return array.join("");
+};
+
+function removeNegative(){
+    let str = display.textContent;
+    let array = Array.from(str);
+    array.splice(0, 1);
+    return array.join("");
+};
+
