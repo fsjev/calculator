77a4d6ffcb359898a03445d404bf7e4e5a165fb2
Jevaughn Vickers <fullstackjev@gmail.com>
2023-07-05 16:21:36 -0400

    refactor code

--- main.js
+++ main.js
@@ -2,20 +2,35 @@
 let firstOperand = 0;
 let secondOperand = 0;
 let operator = "";
-let display = document.querySelector(".display");
-let buttons = document.querySelectorAll(".key");
-let clearAllButton = document.querySelector(".clear-all");
-let clearButton = document.querySelector(".clear");
-let zeroButton = document.querySelector(".zero");
-let equalButton = document.getElementById("equal");
-let plusButton = document.getElementById("add");
-let minusButton = document.getElementById("subtract");
-let multiplyButton = document.getElementById("multiply");
-let divideButton = document.getElementById("divide");
-let negButton = document.getElementById("negative");
-let squareButton = document.getElementById("square");
-let decimalButton = document.getElementById("decimal");
-let keysContainer = document.querySelector(".keys-container");
+const display = document.querySelector(".display");
+const digitButtons = document.querySelectorAll(".digit");
+const operatorButtons = document.querySelectorAll(".operator");
+const clearAllButton = document.querySelector(".clear-all");
+const zeroButton = document.querySelector(".zero");
+const equalButton = document.getElementById("equal");
+const negButton = document.getElementById("negative");
+const squareButton = document.getElementById("square");
+const decimalButton = document.getElementById("decimal");
+const keysContainer = document.querySelector(".keys-container");
+const msg = document.querySelector(".info");
+let is14digits = false;
+let storedDigits = new Array();
+let completeNumberGlobal;
+
+
+
+clearAllButton.addEventListener("click", clear);
+decimalButton.addEventListener("click", clear);
+negButton.addEventListener("click", makeNegative)
+
+digitButtons.forEach(button => button.addEventListener("click", () => appendDigit(button.textContent)));
+
+
+// window.addEventListener("keydown", (e) => {
+//     console.log(e.key);
+// });
+
+
 
 
 function add(x, y){
@@ -38,134 +53,155 @@ function square(x){
     return x ** 2;
 };
 
-function operate(firstOperand, secondOperand, operator){
+function operate(x, y, operator){
+    x = Number(x);
+    y = Number(y);
     switch(operator){
         case "+":
-            return add(firstOperand, secondOperand);
+            return add(x, y);
         case "-":
-            return subtract(firstOperand, secondOperand);
+            return subtract(x, y);
         case "*":
-            return multiply(firstOperand, secondOperand);
+            return multiply(x, y);
         case "/":
-            return divide(firstOperand, secondOperand);
+            return divide(x, y);
     };
 };
 
-buttons.forEach(button => button.addEventListener("click", () => {
-    let liveOperator = keysContainer.getElementsByClassName("operator-click");
-    if(display.textContent.length <= 12){
-        if(button === clearAllButton){
-            clearDisplay();
-            clearOpVariabes();
-        }else if(button === multiplyButton){
-            // if(liveOperator.length === 0){
-            //     multiplyButton.setAttribute("class", "key operator operator-click");
-            // }else{
-            //     multiplyButton.setAttribute("class", "key operator");
-            // }
-            firstOperand = display.textContent;
-        }
-        
-        
-        else if(button === clearButton){
-            if(display.textContent.length === 1){
-                display.textContent = 0;
-            }else{
-                display.textContent = clearLast();
-            }
-        }else if(button === negButton){
-            if(display.textContent.includes("-")){
-                display.textContent = removeNegative();
-            }else{
-                display.textContent = makeNegative();
-            }
-        }else if(button === squareButton){
-            if(display.textContent !== "0"){
-                firstOperand = display.textContent;
-                display.textContent = square(firstOperand);
-
-            }
-        }
-        
-        
-        else if(button === decimalButton){
-            if(!display.textContent.includes(".")){
-                display.textContent = `${display.textContent}${button.textContent}`;
-            }else{
-                display.textContent = clearLast();
-            }
-            
-        }
-        
-        
-        
-        else if(button === equalButton){
-            // outputResult();
-        }else if(button === zeroButton){
-            if(display.textContent === "0"){
-                display.textContent === 0;
-            }else{
-                display.textContent = `${display.textContent}${button.textContent}`;
-            }
-        }else{
-            if(display.textContent === "0"){
-                display.textContent = button.textContent;
-            }else if(display.textContent === "-0"){
-                display.textContent = button.textContent;
-            }
-            
-            
-            else{
-                display.textContent = `${display.textContent}${button.textContent}`;
-            }
-        }
-    }else{
-        if(button === multiplyButton && button === divideButton && button === plusButton && button === minusButton){
-            console.log("yap");
-            // code for processing opers
-        }else if(button === clearAllButton){
-            clearDisplay();
-            clearOpVariabes();
-        }else if(button === clearButton){
-            if(display.textContent.length === 1){
-                display.textContent = 0;
-            }else{
-                display.textContent = clearLast();
-            }
-        }   
-    }
-}));
-
-
-function clearDisplay(){
+function clear(){
     display.textContent = 0;
-};
-
-function clearOpVariabes(){
     firstOperand = 0;
     secondOperand = 0;
     operator = "";
-    
+    storedDigits = [];
+    is14digits = false;
+    setDisplayControls(is14digits);
 };
 
-function clearLast(){
-    let str = display.textContent;
-    let array = Array.from(str);
-    array.pop()
-    return array.join("");
-};
+// function clearLast(){
+//     if(storedDigits.length === 0){
+//         let str = display.textContent;
+//         let array = Array.from(str);
+//         if(array.length === 2 && array.includes("-")){
+//             display.textContent = "0";
+//         }else if(array.length === 1){
+//             display.textContent = "0";
+//         }else{
+//             array.pop();
+//             display.textContent = array.join("");
+//         }
+//     }else{
+//         completeNumberGlobal.pop();
+//         let setdisplay = completeNumberGlobal.slice(-13);
+//         display.textContent =  setdisplay.join("");
+//         is14digits = false;
+//         setDisplayControls(is14digits);
+//     }
+// };
 
 function makeNegative(){
     let str = display.textContent;
     let array = Array.from(str);
-    array.splice(0, 0, "-");
-    return array.join("");
+    if(str.includes("-")){
+        array.splice(0, 1);
+        display.textContent = array.join("");
+    }else{
+        array.splice(0, 0, "-");
+        display.textContent = array.join("");
+    }
 };
 
-function removeNegative(){
-    let str = display.textContent;
-    let array = Array.from(str);
-    array.splice(0, 1);
-    return array.join("");
+function appendDigit(digit){
+    if (display.textContent.length === 13){
+        is14digits = true;
+        updateLongNum(digit);
+        setDisplayControls(is14digits);
+    }else{
+        is14digits = false;
+        setDisplayControls(is14digits);
+        if(display.textContent === "0"){
+            display.textContent = digit;
+        }else{
+            display.textContent = `${display.textContent}${digit}`;
+        }
+    }
 };
+
+
+function updateLongNum(digit){
+    let displayValue = `${display.textContent}${digit}`;
+    let array = Array.from(displayValue);
+    let firstNum = array.shift();
+    let displayArray = Array.from(display.textContent);
+    if(storedDigits.length === 0){
+        let completeNumber = storeLongNum(firstNum, displayValue);
+        completeNumberGlobal = completeNumber;
+        display.textContent =  array.join("");
+    }else{
+        let displayLastElemIndex = completeNumberGlobal.lastIndexOf(displayArray[12]);
+        if(displayLastElemIndex === completeNumberGlobal.length -1){
+            let completeNumber = storeLongNum(firstNum, displayValue);
+            completeNumberGlobal = completeNumber;
+            display.textContent =  array.join("");
+        }else{
+            let setdisplay = completeNumberGlobal.slice(-13);
+            display.textContent =  setdisplay.join("");
+            displayValue = `${display.textContent}${digit}`;
+            array = Array.from(displayValue);
+            firstNum = array.shift();
+            completeNumber = storeLongNum(firstNum, displayValue);
+            completeNumberGlobal = completeNumber;
+            display.textContent =  array.join("");
+        }  
+    }
+
+    
+        
+    
+};
+
+function storeLongNum(firstNum, displayValue){
+    storedDigits.push(firstNum);
+    let restOfNum = Array.from(displayValue);
+    let restOfNumToJoin = restOfNum.slice(1);
+    return storedDigits.concat(restOfNumToJoin);
+};
+
+function setDisplayControls(is14digits){
+    if(is14digits){
+        msg.style.visibility = "visible";
+        window.addEventListener("keydown", controlDisplay);
+    }else{
+        msg.style.visibility = "hidden";
+        window.removeEventListener("keydown", controlDisplay);
+    }
+};
+
+function controlDisplay(e){
+    let displayArray = Array.from(display.textContent);
+    if(e.key === "ArrowLeft"){
+        let displayFirstElemIndex = completeNumberGlobal.indexOf(displayArray[0]);
+        if(displayFirstElemIndex === 0){
+            alert("Can't move left!")
+        }else{
+            let leftStepIndex = displayFirstElemIndex - 1;
+            let leftStepElem = completeNumberGlobal.join("").charAt(leftStepIndex);
+            displayArray.unshift(leftStepElem);
+            displayArray.pop();
+            display.textContent = displayArray.join("");
+        }
+    }else if(e.key === "ArrowRight"){
+        let displayLastElemIndex = completeNumberGlobal.lastIndexOf(displayArray[12]);
+        if(displayLastElemIndex === completeNumberGlobal.length -1){
+            alert("Can't move right!")
+        }else{
+            let rightStepIndex = displayLastElemIndex + 1;
+            let rightStepElem = completeNumberGlobal.join("").charAt(rightStepIndex);
+            displayArray.push(rightStepElem);
+            displayArray.splice(0,1);
+            display.textContent = displayArray.join("");
+        }
+    }
+};
+
 
